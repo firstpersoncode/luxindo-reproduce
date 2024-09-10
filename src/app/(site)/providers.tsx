786756ceadef33cfed7a10987a5b2188d91366e4
firstpersoncode <fpcdatadriver@gmail.com>
2024-09-10 20:57:53 +0800

    bump

--- src/app/(site)/providers.tsx
+++ src/app/(site)/providers.tsx
@@ -1,5 +1,5 @@
 'use client'
-import { AppRouterCacheProvider } from '@mui/material-nextjs/v13-appRouter';
+import { AppRouterCacheProvider } from '@mui/material-nextjs/v14-appRouter';
 import { ThemeProvider } from '@mui/material/styles'
 import theme from './theme';
 
