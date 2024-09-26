d270212b81147cb3fd39863334c1c361a35de1b8
firstpersoncode <fpcdatadriver@gmail.com>
2024-09-27 03:35:24 +0800

    wip: search feature

--- src/modules/backend/api/search-properties.api.ts
+++ src/modules/backend/api/search-properties.api.ts
@@ -32,41 +32,19 @@ export const searchPropertiesApi = async (req: NextRequest) => {
 
   if (query.area_2) {
     const areas = query.area_2.split('|')
-    const options = LOCATIONS
-    const parent = options.find((o) => o.children?.find((c) => areas.includes(c.value)))
-    const selectedParent = parent ? or.find((a: any) => a.area_1.equals === parent.value) : null
-    if (selectedParent) {
-      or = or.filter((a: any) => a.area_1.equals !== selectedParent.area_1.equals)
-      areas.forEach((area) => {
-        or.push({ area_2: { equals: area } })
-      })
-    }
+    areas.forEach((area) => {
+      const options = LOCATIONS
+      const parent = options.find((o) => o.children?.find((c) => c.value === area))
+      or = or.filter((a: any) => a.area_1?.equals !== parent?.value)
+      or.push({ area_2: { equals: area } })
+    })
   }
 
   if (or.length > 0) where.and.push({ or })
 
-  // if (query.area_1 || query.area_2) {
-  //   const or = []
-  //   if (query.area_1) {
-  //     const areas = query.area_1.split('|')
-  //     or.push({ area_1: { in: areas } })
-  //   }
-  //   if (query.area_2) {
-  //     const areas = query.area_2.split('|')
-  //     or.push({ area_2: { in: areas } })
-  //   }
-  //   where.and.push({ or })
-  // }
-
   if (query.sku) where.and.push({ sku: { equals: query.sku } })
   if (query.price_start) where.and.push({ price: { greater_than_equal: query.price_start } })
   if (query.price_end) where.and.push({ price: { less_than_equal: query.price_end } })
-
-  // if (where.and.length === 1) {
-  //   const fieldName = Object.keys(where.and[0])[0]
-  //   where[fieldName] = where.and[0][fieldName]
-  //   delete where.and
-  // }
 
   const properties = await payload.find({
     collection: 'properties',
